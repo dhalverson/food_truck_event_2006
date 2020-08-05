16d568466d97f366b133231ddcb861c708fba692
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-05 09:07:58 -0600

    Adds food truck stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -25,6 +25,9 @@ class FoodTruckTest < Minitest::Test
     assert_equal ({}), food_truck.inventory
 
     assert_equal 0, food_truck.check_stock(item1)
+    food_truck.stock(item1, 30)
+
+    assert_equal ({item1 => 30}), food_truck.inventory
   end
 
 
