15cdcc53a2e14d95c71f90f877c79ef9cafa537c
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-05 09:38:25 -0600

    Add event total inventory method

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,17 @@ class Event
     end
   end
 
+  def total_inventory
+    result = {}
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, amount|
+        summed_quantity = food_trucks_that_sell(item).sum do |truck|
+          truck.check_stock(item)
+        end
+        result[item] = {quantity: summed_quantity, food_trucks: food_trucks_that_sell(item)}
+      end
+    end
+    result
+  end
+
 end
